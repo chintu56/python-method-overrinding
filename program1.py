b33c886f3a759ecc901373003c64fcf215f460a9
chintu56 <155710174+chintu56@users.noreply.github.com>
2024-12-14 13:40:06 +0530

    Add files via upload

---
+++ program1.py
@@ -0,0 +1,13 @@
+class bharath:
+    def style(self):
+        print("normal")
+    
+    def language(self):
+        print("telugu")
+class chintu(bharath):
+    def language(self):
+        print("english")
+  
+c=chintu()
+c.language()
+c.style()
---
+++ program2.py
@@ -0,0 +1,5 @@
+class parent:
+    def __init__(self):
+        self.value="inside parent"
+    def show(self):
+        print(self.value)
